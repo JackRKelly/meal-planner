fa4f807750655d0c0bd92a1b53d7f9c0d783b5df
JackRKelly <kcjackkelly@gmail.com>
2021-01-30 12:39:42 -0600

    Added mock repository for basic implementation of meal interface

--- server/Data/IMealPlanner.cs
+++ server/Data/IMealPlanner.cs
@@ -1,5 +1,11 @@
-namespace MealPlanner.Data {
-  public interface IMealPlanner {
-    
+using System.Collections.Generic;
+using MealPlanner.Models;
+
+namespace MealPlanner.Data
+{
+  public interface IMealPlanner
+  {
+    IEnumerable<Meal> GetMeals();
+    Meal GetMealById(int id);
   }
 }
---
+++ server/Data/MockMealPlanner.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using MealPlanner.Models;
+
+namespace MealPlanner.Data
+{
+  public class MockMeal : IMealPlanner
+  {
+    public IEnumerable<Meal> GetMeals()
+    {
+      var meals = new List<Meal>
+      {
+        new Meal {
+          Id = 0,
+          Name = "Meal from list",
+          Image = "https://google.com",
+          Instructions = new string[] { "Step 1", "Step 2" },
+          Ingredients = new string[] { "Ingredient 1", "Ingredient 2" }
+        },
+        new Meal {
+          Id = 1,
+          Name = "Meal from list",
+          Image = "https://google.com",
+          Instructions = new string[] { "Step 3", "Step 4" },
+          Ingredients = new string[] { "Ingredient 3", "Ingredient 4" }
+        }
+      };
+
+      return meals;
+    }
+
+    public Meal GetMealById(int id)
+    {
+      return new Meal
+      {
+        Id = 0,
+        Name = "Meal",
+        Image = "https://google.com",
+        Instructions = new string[] { "Step 1", "Step 2" },
+        Ingredients = new string[] { "Ingredient 1", "Ingredient 2" }
+      };
+    }
+  }
+}
