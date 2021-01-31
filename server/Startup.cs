cf4dfd62a3bddadff9ae849fae6107547e7378eb
JackRKelly <kcjackkelly@gmail.com>
2021-01-31 09:05:07 -0600

    Added auto mapper dependency

--- server/Startup.cs
+++ server/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using MealPlanner.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -36,6 +37,8 @@ namespace MealPlanner
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "Meal Planner", Version = "v1" });
       });
+
+      services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
       services.AddScoped<IMealPlanner, SqlMealPlanner>();
     }
