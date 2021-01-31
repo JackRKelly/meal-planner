9852cb11aedc092aa7d3221b73793a075170f3e8
JackRKelly <kcjackkelly@gmail.com>
2021-01-30 18:17:31 -0600

    Added sql meal planner

---
+++ server/Data/SqlMealPlanner.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using MealPlanner.Models;
+
+namespace MealPlanner.Data
+{
+  public class SqlMealPlanner : IMealPlanner
+  {
+    public IEnumerable<Meal> GetAllMeals()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public Meal GetMealById(int id)
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
