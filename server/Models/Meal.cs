37f3bf24b8491e48f4cda07656e1139a38481dc3
JackRKelly <kcjackkelly@gmail.com>
2021-01-30 18:00:04 -0600

    Added decorators for Meal model

--- server/Models/Meal.cs
+++ server/Models/Meal.cs
@@ -1,11 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MealPlanner.Models
 {
   public class Meal
   {
+    [Key]
     public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
     public string Name { get; set; }
+
+    [Required]
     public string Image { get; set; }
+
+    [Required]
     public string[] Instructions { get; set; }
+
+    [Required]
     public string[] Ingredients { get; set; }
   }
 }
