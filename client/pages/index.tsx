209d1fa628d70166c2eb0c1dd7a74550963c6b50
JackRKelly <kcjackkelly@gmail.com>
2021-02-01 10:49:36 -0600

    Added babel config for next ssr

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,15 +1,28 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import React from "react";
+import Layout from "../components/Layout";
+import {
+  Content,
+  Section,
+  SectionHeader,
+  SectionText,
+} from "../components/Styled";
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+  <Layout title="Home | Meal Planner">
+    <Content>
+      <Section>
+        <SectionHeader>Meal Planner</SectionHeader>
+        <SectionText>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit iste,
+          inventore dolor praesentium cum consectetur et esse doloremque commodi
+          magnam aliquid, expedita provident a. Eligendi neque corrupti unde
+          ducimus dicta. Minima rerum necessitatibus ad tempora magnam
+          voluptatem quaerat exercitationem sapiente sit illo cum possimus et
+          rem, consequuntur voluptatibus deleniti esse doloremque voluptates.
+        </SectionText>
+      </Section>
+    </Content>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
